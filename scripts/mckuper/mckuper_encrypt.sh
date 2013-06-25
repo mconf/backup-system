104f4de9276c86732a6e367813dad7e3f249661a
mrcruz <brcruz@gmail.com>
2013-06-25 16:58:29 -0300

    adding #!/bin/bash to scripts

--- scripts/mckuper/mckuper_encrypt.sh
+++ scripts/mckuper/mckuper_encrypt.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Encrypt directly to backup folder
 sudo tar -zcf - $ENCRYPT_SOURCE_PATH | openssl aes-256-cbc  -k $ENCRYPT_SECRET -salt -out $ENCRYPT_DEST_PATH/$ENCRYPT_ID-`date +%F-%Hh%M`.tar.gz.aes  
 
--- scripts/mckuper/mckuper_rotate.sh
+++ scripts/mckuper/mckuper_rotate.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 BACKUP_COUNT=`ls $ROTATE_FOLDER/$ROTATE_FILTER | wc -l`
 while [ "${BACKUP_COUNT}" -gt "${ROTATE_MAX}" ]; do
   OLDEST=`ls $ROTATE_FOLDER/$ROTATE_FILTER | head -1`
